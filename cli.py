8cecad199f779780ed4c071c2a817689aa915dd6
laobubu <laobubu@gmail.com>
2016-05-22 21:44:26 +0800

    cli

---
+++ cli.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  CLI
+#
+
+import config, user
+import sys
+import getpass
+
+def _print_help():
+    print(
+'''
+Usage:
+
+ user list
+ user add {username}
+ user add {username} {password}
+ user del {username}
+ user passwd {username}
+ user passwd {username} {password}
+''')
+    sys.exit(0)
+
+if len(sys.argv) < 3:
+    _print_help()
+
+scope = sys.argv[1]
+action = sys.argv[2]
+argv = sys.argv[3:]
+
+if scope == 'user':
+    user.cache_all();
+    if   action == 'list':
+        for un in user._USER_CACHE.keys():
+            print(un)
+    elif action == 'add':
+        username = argv[0] if len(argv) > 0 else raw_input('Username: ')
+        password = argv[1] if len(argv) > 1 else getpass.getpass()
+        user.User(username, password).write()
+    elif action == 'del':
+        for un in argv:
+            os.remove(user.user_filename(un))
+    elif action == 'passwd':
+        username = argv[0] if len(argv) > 0 else raw_input('Username: ')
+        password = argv[1] if len(argv) > 1 else getpass.getpass()
+        u = user.open(username)
+        u.salted_password = user.salt_password(password)
+        u.write()
--- user.py
+++ user.py
@@ -5,21 +5,33 @@
 #   Including User class and reading/writing implementations
 #
 
-import re, json, time
+import re, os, json, time
 import config
 from hashlib import md5
 
 class User:
-    def __init__(self, username):
-        self.username = username
+    def __init__(self, src, password=None):
+        '''
+        Create one User instance from a Dict or String object.
+        '''
+        self.username = ''
         self.salted_password = ''
-        self.join_since = time.time()
+        self.id = 0
+        self.join_since = 0
         self.name = ''
         self.comment = ''
-        self.port = 0
+        if not type(src) in [str, unicode]:
+            for k in self.__dict__.keys():
+                setattr(self, k, src[k])
+        else:
+            self.username = src
+            self.salted_password = salt_password(password)
+            self.join_since = time.time()
+            self.id = next_id()
         
     def write(self):
         _USER_CACHE[self.username] = self
+        update_lookup_table()
         f = file(user_filename(self.username), 'w')
         json.dump(self.__dict__, f)
         f.close()
@@ -29,17 +41,49 @@ user_filename = lambda username: config.USER_ROOT + '/' + username + '.json'
 
 # Every user info will be cached here to reduce harddisk IO
 _USER_CACHE = {}
+_USER_CACHE_ID = {}
 
 def open(username):
+    '''
+    Get an existing User() instance.
+    '''
     try:
         if not _USER_CACHE.has_key(username):
             f = file(user_filename(username), 'r')
             j = json.load(f)
-            u = User(username)
-            for k in u.__dict__.keys():
-                u[k] = j[k]
+            u = User(j)
             _USER_CACHE[username] = u
             f.close()
         return _USER_CACHE[username]
     except:
         return None
+
+def cache_all():
+    '''
+    Load and cache all existing user files. 
+    '''
+    for fn in os.listdir(config.USER_ROOT):
+        if fn[-5:] == '.json':
+            open(fn[:-5])
+    update_lookup_table()
+
+def update_lookup_table():
+    '''
+    Update ID -> User lookup table.
+    '''
+    global _USER_CACHE_ID
+    _USER_CACHE_ID = {}
+    for i in _USER_CACHE:
+        u = _USER_CACHE[i]
+        _USER_CACHE_ID[u.id] = u
+
+def next_id():
+    '''
+    Get next avaliable ID.
+    '''
+    ids = _USER_CACHE_ID.keys()
+    ids.sort()
+    l = len(ids)
+    for i in range(l):
+        if ids[i] > i: return i
+    return l
